3823c0297beaf87665de39de25d40614289865cc
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-06 09:32:43 +0700

    Update profiles

--- src/layout/Dashboard/Header/HeaderContent/Profile/ProfileTab.tsx
+++ src/layout/Dashboard/Header/HeaderContent/Profile/ProfileTab.tsx
@@ -1,28 +1,40 @@
-import { MouseEvent, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MouseEvent, useState } from "react";
 
 // material-ui
-import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import {
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
 // assets
-import { EditOutlined, LogoutOutlined } from '@mui/icons-material';
+import { LogoutOutlined } from "@mui/icons-material";
 
 // ==============================|| HEADER PROFILE - PROFILE TAB ||============================== //
 
 const ProfileTab = ({ handleLogout }: { handleLogout: () => void }) => {
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
-  const handleListItemClick = (event: MouseEvent<HTMLDivElement>, index: number) => {
+  const handleListItemClick = (
+    event: MouseEvent<HTMLDivElement>,
+    index: number
+  ) => {
     setSelectedIndex(index);
   };
 
   return (
-    <List component="nav" sx={{ p: 0, '& .MuiListItemIcon-root': { minWidth: 32 } }}>
-      <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
+    <List
+      component="nav"
+      sx={{ p: 0, "& .MuiListItemIcon-root": { minWidth: 32 } }}
+    >
+      {/* <ListItemButton selected={selectedIndex === 0} onClick={(event) => handleListItemClick(event, 0)}>
         <ListItemIcon>
           <EditOutlined />
         </ListItemIcon>
         <ListItemText primary="Edit Profile" />
-      </ListItemButton>
+      </ListItemButton> */}
       <ListItemButton selected={selectedIndex === 2} onClick={handleLogout}>
         <ListItemIcon>
           <LogoutOutlined />
--- src/layout/Dashboard/Header/HeaderContent/Profile/index.tsx
+++ src/layout/Dashboard/Header/HeaderContent/Profile/index.tsx
@@ -1,22 +1,32 @@
-import { useRef, useState } from 'react';
-import { useNavigate } from 'react-router';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router";
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
-import { Box, ButtonBase, CardContent, ClickAwayListener, Grid, Paper, Popper, Stack, Tooltip, Typography } from '@mui/material';
+import { useTheme } from "@mui/material/styles";
+import {
+  Box,
+  ButtonBase,
+  CardContent,
+  ClickAwayListener,
+  Grid,
+  Paper,
+  Popper,
+  Stack,
+  Typography,
+} from "@mui/material";
 
 // project import
-import ProfileTab from './ProfileTab';
-import Avatar from '@/components/@extended/Avatar';
-import MainCard from '@/components/MainCard';
-import Transitions from '@/components/@extended/Transitions';
-import IconButton from '@/components/@extended/IconButton';
+import ProfileTab from "./ProfileTab";
+import Avatar from "@/components/@extended/Avatar";
+import MainCard from "@/components/MainCard";
+import Transitions from "@/components/@extended/Transitions";
 
-import useAuth from '@/hooks/useAuth';
-import { ThemeMode } from '@/config';
+import useAuth from "@/hooks/useAuth";
+import { ThemeMode } from "@/config";
 
 // assets
-import { LogoutOutlined, AccountCircleOutlined } from '@mui/icons-material';
+import { AccountCircleOutlined } from "@mui/icons-material";
 
 // ==============================|| HEADER CONTENT - PROFILE ||============================== //
 
@@ -25,13 +35,14 @@ const Profile = () => {
   const navigate = useNavigate();
 
   const { logout, user } = useAuth();
+
   const handleLogout = async () => {
     try {
       await logout();
       navigate(`/`, {
         state: {
-          from: ''
-        }
+          from: "",
+        },
       });
     } catch (err) {
       console.error(err);
@@ -51,34 +62,49 @@ const Profile = () => {
     setOpen(false);
   };
 
-  const iconBackColorOpen = theme.palette.mode === ThemeMode.DARK ? 'background.default' : 'grey.100';
+  const iconBackColorOpen =
+    theme.palette.mode === ThemeMode.DARK ? "background.default" : "grey.100";
+
+  const dataUser = user?.data;
+
+  const username = window.localStorage.getItem("username");
 
   return (
     <Box sx={{ flexShrink: 0, ml: 0.75 }}>
       <ButtonBase
         sx={{
           p: 0.25,
-          bgcolor: open ? iconBackColorOpen : 'transparent',
+          bgcolor: open ? iconBackColorOpen : "transparent",
           borderRadius: 1,
-          '&:hover': { bgcolor: theme.palette.mode === ThemeMode.DARK ? 'secondary.light' : 'secondary.lighter' },
-          '&:focus-visible': {
+          "&:hover": {
+            bgcolor:
+              theme.palette.mode === ThemeMode.DARK
+                ? "secondary.light"
+                : "secondary.lighter",
+          },
+          "&:focus-visible": {
             outline: `2px solid ${theme.palette.secondary.dark}`,
-            outlineOffset: 2
-          }
+            outlineOffset: 2,
+          },
         }}
         aria-label="open profile"
         ref={anchorRef}
-        aria-controls={open ? 'profile-grow' : undefined}
+        aria-controls={open ? "profile-grow" : undefined}
         aria-haspopup="true"
         onClick={handleToggle}
       >
-        <Stack direction="row" spacing={1.25} alignItems="center" sx={{ p: 0.5 }}>
+        <Stack
+          direction="row"
+          spacing={1.25}
+          alignItems="center"
+          sx={{ p: 0.5 }}
+        >
           <Avatar alt="profile user" size="sm">
             <AccountCircleOutlined />
           </Avatar>
-          <Typography variant="subtitle1" sx={{ textTransform: 'capitalize' }}>
-            {user?.name}
-          </Typography>
+          {/* <Typography variant="subtitle1" sx={{ textTransform: "capitalize" }}>
+            {dataUser?.nama || "-"}
+          </Typography> */}
         </Stack>
       </ButtonBase>
       <Popper
@@ -91,51 +117,78 @@ const Profile = () => {
         popperOptions={{
           modifiers: [
             {
-              name: 'offset',
+              name: "offset",
               options: {
-                offset: [0, 9]
-              }
-            }
-          ]
+                offset: [0, 9],
+              },
+            },
+          ],
         }}
       >
         {({ TransitionProps }) => (
-          <Transitions type="grow" position="top-right" in={open} {...TransitionProps}>
+          <Transitions
+            type="grow"
+            position="top-right"
+            in={open}
+            {...TransitionProps}
+          >
             <Paper
               sx={{
                 boxShadow: theme.customShadows?.z1,
                 width: 290,
                 minWidth: 240,
                 maxWidth: 290,
-                [theme.breakpoints.down('md')]: {
-                  maxWidth: 250
-                }
+                [theme.breakpoints.down("md")]: {
+                  maxWidth: 250,
+                },
               }}
             >
               <ClickAwayListener onClickAway={handleClose}>
-                <MainCard elevation={0} border={false} content={false}>
+                <MainCard
+                  elevation={0}
+                  border={false}
+                  content={false}
+                  showButton={false}
+                >
                   <CardContent sx={{ px: 2.5, pt: 3 }}>
-                    <Grid container justifyContent="space-between" alignItems="center">
+                    <Grid
+                      container
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
                       <Grid item>
-                        <Stack direction="row" spacing={1.25} alignItems="center">
-                          <Avatar alt="profile user" sx={{ width: 32, height: 32 }}>
+                        <Stack
+                          direction="row"
+                          spacing={1.25}
+                          alignItems="center"
+                        >
+                          <Avatar
+                            alt="profile user"
+                            sx={{ width: 32, height: 32 }}
+                          >
                             <AccountCircleOutlined />
                           </Avatar>
                           <Stack>
-                            <Typography variant="h6">{user?.name}</Typography>
-                            <Typography variant="body2" color="textSecondary">
-                              UI/UX Designer
+                            <Typography variant="h6">
+                              {username || "-"}
                             </Typography>
+                            {/* <Typography variant="body2" color="textSecondary">
+                              {dataUser?.jabatan || "-"}
+                            </Typography> */}
                           </Stack>
                         </Stack>
                       </Grid>
-                      <Grid item>
+                      {/* <Grid item>
                         <Tooltip title="Logout">
-                          <IconButton size="large" sx={{ color: 'text.primary' }} onClick={handleLogout}>
+                          <IconButton
+                            size="large"
+                            sx={{ color: "text.primary" }}
+                            onClick={handleLogout}
+                          >
                             <LogoutOutlined />
                           </IconButton>
                         </Tooltip>
-                      </Grid>
+                      </Grid> */}
                     </Grid>
                   </CardContent>
                   <ProfileTab handleLogout={handleLogout} />
