b6dba7970c808bd1dd0280b3593a8714c2e4349e
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-06 09:32:16 +0700

    Add LogoMain on components

--- src/components/logo/LogoMain.tsx
+++ src/components/logo/LogoMain.tsx
@@ -1,18 +1,15 @@
-// assets
-
-// ==============================|| LOGO SVG ||============================== //
+import Logo from "../../assets/logo.png";
+import { StyleSheet } from "@react-pdf/renderer";
 
 const LogoMain = () => {
-  return (
-    <>
-      {/* <img
-        src={''}
-        alt="CIS Logo"
-        width="140"
-      /> */}
-      <h5>CIS</h5>
-    </>
-  );
+  return <img style={styles.logo} src={Logo} />;
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 100,
+    height: 60,
+  },
+});
 
 export default LogoMain;
