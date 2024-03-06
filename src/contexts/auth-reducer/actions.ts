2655db39624eb4e8e5b38c326954ff7fb921650d
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-06 09:29:17 +0700

    Update actions on contexts

--- src/contexts/auth-reducer/actions.ts
+++ src/contexts/auth-reducer/actions.ts
@@ -1,3 +1,4 @@
 // action - account reducer
-export const LOGIN = '@auth/LOGIN';
-export const LOGOUT = '@auth/LOGOUT';
+export const LOGIN = "@auth/LOGIN";
+export const LOGOUT = "@auth/LOGOUT";
+export const IDEMPLOYEE = "@auth/IDEMPLOYEE";
