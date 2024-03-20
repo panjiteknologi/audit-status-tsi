2c52381fda0c53e93abdeaeb199694764a68a15b
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-20 08:52:18 +0700

    Add icon on input dropdown

--- src/components/InputDropdown.tsx
+++ src/components/InputDropdown.tsx
@@ -3,14 +3,17 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { ListMenuStatus } from "@/types/Menu";
-import { useTheme } from "@mui/material";
+import { InputAdornment, OutlinedInput, useTheme } from "@mui/material";
 import { ThemeMode } from "@/config";
+import { ReactNode } from "react";
 
 interface InputDropdown {
   title: string;
   selectMenu: ListMenuStatus[];
   value: string;
   setValue: (selected: string) => void;
+  disabled: boolean;
+  rightIcon: ReactNode;
 }
 
 const InputDropdown = ({
@@ -18,6 +21,8 @@ const InputDropdown = ({
   selectMenu,
   value,
   setValue,
+  disabled,
+  rightIcon,
 }: InputDropdown) => {
   const theme = useTheme();
 
@@ -26,7 +31,7 @@ const InputDropdown = ({
   };
 
   return (
-    <FormControl sx={{ minWidth: "90%" }} size="small">
+    <FormControl sx={{ width: "100%" }} size="small">
       <InputLabel id="demo-select-small-label" sx={{ marginTop: 0.5 }}>
         {title}
       </InputLabel>
@@ -36,15 +41,26 @@ const InputDropdown = ({
         value={value}
         label={title}
         onChange={handleChange}
-        sx={{
-          height: 40,
-          borderRadius: 1,
-          borderWidth: "1px solid",
-          borderColor:
-            theme.palette.mode === ThemeMode.DARK
-              ? theme.palette.divider
-              : theme.palette.grey.A800,
-        }}
+        disabled={disabled}
+        input={
+          <OutlinedInput
+            endAdornment={
+              disabled && (
+                <InputAdornment position="end">{rightIcon}</InputAdornment>
+              )
+            }
+            sx={{
+              backgroundColor: !disabled ? "white" : theme.palette.grey.A800,
+              height: 40,
+              borderRadius: 1,
+              borderWidth: "1px solid",
+              borderColor:
+                theme.palette.mode === ThemeMode.DARK
+                  ? theme.palette.divider
+                  : theme.palette.grey.A800,
+            }}
+          />
+        }
       >
         {selectMenu?.map((items: ListMenuStatus, index: number) => (
           <MenuItem key={index} value={items?.status_id}>
