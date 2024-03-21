3da9d897b77cc3a90626f2566ca755314b5e7fad
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-21 15:22:22 +0700

    Update UI payslip document

--- src/sections/payslip/pdf/BenefitInformation.tsx
+++ src/sections/payslip/pdf/BenefitInformation.tsx
@@ -1,6 +1,7 @@
 import { User } from "@/types/User";
 import { formatIdr } from "@/utils/formatIdr";
 import { Text, View, StyleSheet } from "@react-pdf/renderer";
+import { ReactNode } from "react";
 
 interface BenefitInformationProps {
   data: User;
@@ -9,6 +10,18 @@ interface BenefitInformationProps {
   totalPendapatan: number;
   totalPotongan: number;
 }
+
+interface PropsGrid {
+  children: ReactNode;
+}
+
+const GridItem = ({ children }: PropsGrid) => (
+  <View style={styles.gridItem}>{children}</View>
+);
+
+const Grid = ({ children }: PropsGrid) => (
+  <View style={styles.gridContainer}>{children}</View>
+);
 
 const BenefitInformation = ({
   data,
@@ -22,82 +35,155 @@ const BenefitInformation = ({
       <View style={styles.content}>
         <Text style={styles.txt}>Pendapatan</Text>
         <View style={styles.informationContent}>
-          <View style={{ width: "60%" }}>
-            <Text style={styles.titleItem}>Jumlah Masuk Kerja</Text>
-            <Text style={styles.titleItem}>Gaji Prorate Diterima</Text>
-            <Text style={styles.titleItem}>Total Lembur</Text>
-            <Text style={styles.titleItem}>Total Uang Lembur</Text>
-            <Text style={styles.titleItem}>Insentive</Text>
-            <Text style={styles.titleItem}>Tunjangan Transport</Text>
-            <Text style={styles.titleItem}>THR</Text>
-            <Text style={styles.titleItem}>Kekurangan Bayar Bulan Lalu</Text>
-            <Text style={styles.titleItem}>TOTAL PENDAPATAN</Text>
-          </View>
-          <View style={{ width: "40%" }}>
-            <Text style={styles.text}>
-              {data?.total_masuk_kerja || "0"} Hari
-            </Text>
-            <Text style={styles.text}>{formatIdr(gajiProrate) || "0"}</Text>
-            <Text style={styles.text}>{data?.lembur || "0"} Jam</Text>
-            <Text style={styles.text}>{formatIdr(totalLembur) || "0"}</Text>
-            <Text style={styles.text}>{formatIdr(data?.insentive) || "0"}</Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.tunjangan_transport) || "0"}
-            </Text>
-            <Text style={styles.text}>{formatIdr(data?.thr) || "0"}</Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.kekurangan_bayar_bulan_lalu) || "0"}
-            </Text>
-            <Text style={styles.text}>{formatIdr(totalPendapatan) || "0"}</Text>
-          </View>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>Jumlah Masuk Kerja</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.total_masuk_kerja || "0"} Hari
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Gaji Prorate Diterima</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>{formatIdr(gajiProrate) || "0"}</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Total Lembur</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>{data?.lembur || "0"} Jam</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Total Uang Lembur</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>{formatIdr(totalLembur) || "0"}</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Insentive</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.insentive) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Tunjangan Transport</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.tunjangan_transport) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>THR</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>{formatIdr(data?.thr) || "0"}</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Kekurangan Bayar Bulan Lalu</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.kekurangan_bayar_bulan_lalu) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>TOTAL PENDAPATAN</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(totalPendapatan) || "0"}
+              </Text>
+            </GridItem>
+          </Grid>
         </View>
       </View>
       <View style={styles.content}>
         <Text style={styles.txt}>Potongan</Text>
-
         <View style={styles.informationContent}>
-          <View style={{ width: "70%" }}>
-            <Text style={styles.titleItem}>
-              Potongan Admin {data?.rekening}
-            </Text>
-            <Text style={styles.titleItem}>Potongan BPJS Ketenagakerjaan</Text>
-            <Text style={styles.titleItem}>Potongan BPJS Kesehatan</Text>
-            <Text style={styles.titleItem}>Potongan Iuran</Text>
-            <Text style={styles.titleItem}>Potongan Ketidakhadiran</Text>
-            <Text style={styles.titleItem}>Potongan Tidak Absen</Text>
-            <Text style={styles.titleItem}>
-              Potongan Kelebihan Bayar Bulan Lalu
-            </Text>
-            <Text style={styles.titleItem}>Potongan Pinjaman</Text>
-            <Text style={styles.titleItem}>TOTAL POTONGAN</Text>
-          </View>
-          <View style={{ width: "30%" }}>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_admin_bank) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_bpjs_tk) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_bpjs_kesehatan) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_iuran) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_ketidak_hadiran) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_tidak_absen) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_kelebihan_bayar_bulan_lalu) || "0"}
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.potongan_pinjaman) || "0"}
-            </Text>
-            <Text style={styles.text}>{formatIdr(totalPotongan) || "0"}</Text>
-          </View>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>
+                Potongan Admin {data?.rekening}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_admin_bank) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>
+                Potongan BPJS Ketenagakerjaan
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_bpjs_tk) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Potongan BPJS Kesehatan</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_bpjs_kesehatan) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Potongan Iuran</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_iuran) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Potongan Ketidakhadiran</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_ketidak_hadiran) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Potongan Tidak Absen</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_tidak_absen) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>
+                Potongan Kelebihan Bayar Bulan Lalu
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_kelebihan_bayar_bulan_lalu) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>Potongan Pinjaman</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.potongan_pinjaman) || "0"}
+              </Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.titleItem}>TOTAL POTONGAN</Text>
+            </GridItem>
+            <GridItem>
+              <Text style={styles.text}>{formatIdr(totalPotongan) || "0"}</Text>
+            </GridItem>
+          </Grid>
         </View>
       </View>
     </View>
@@ -106,21 +192,18 @@ const BenefitInformation = ({
 
 const styles = StyleSheet.create({
   headerContainer: {
-    marginTop: 10,
+    marginVertical: 14,
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
-    marginBottom: 16,
   },
   content: {
-    flexDirection: "column",
-    width: "50%",
-    marginTop: 10,
+    width: "100%",
   },
   informationContent: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 4,
+    marginTop: 6,
   },
   txt: {
     fontSize: "10px",
@@ -134,7 +217,6 @@ const styles = StyleSheet.create({
     fontSize: "8px",
     textTransform: "capitalize",
     fontWeight: "bold",
-    marginBottom: 2,
   },
   text: {
     fontWeight: "semibold",
@@ -143,7 +225,17 @@ const styles = StyleSheet.create({
     boxShadow: "none",
     fontSize: "8px",
     textTransform: "capitalize",
-    marginBottom: 2,
+    marginRight: 8,
+    width: "45%",
+  },
+  gridContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  gridItem: {
+    width: "48%",
   },
 });
 
--- src/sections/payslip/pdf/EmployeeInfo.tsx
+++ src/sections/payslip/pdf/EmployeeInfo.tsx
@@ -2,10 +2,22 @@ import { User } from "@/types/User";
 import { formatIdr } from "@/utils/formatIdr";
 import { Text, View, StyleSheet } from "@react-pdf/renderer";
 import dayjs from "dayjs";
+import { ReactNode } from "react";
 
 interface EmployeeInfoProps {
   data: User;
 }
+interface PropsGrid {
+  children: ReactNode;
+}
+
+const GridItem = ({ children }: PropsGrid) => (
+  <View style={styles.gridItem}>{children}</View>
+);
+
+const Grid = ({ children }: PropsGrid) => (
+  <View style={styles.gridContainer}>{children}</View>
+);
 
 const EmployeeInfo = ({ data }: EmployeeInfoProps) => {
   return (
@@ -13,46 +25,100 @@ const EmployeeInfo = ({ data }: EmployeeInfoProps) => {
       <Text style={styles.txt}>Kepada Yth</Text>
       <View style={styles.content}>
         <View style={styles.informationContent}>
-          <View style={{ width: "60%" }}>
-            <Text style={styles.titleItem}>Nama</Text>
-            <Text style={styles.titleItem}>Jabatan</Text>
-            <Text style={styles.titleItem}>Lokasi</Text>
-            <Text style={styles.titleItem}>Periode</Text>
-          </View>
-          <View style={{ width: "40%" }}>
-            <Text style={styles.text}>{data?.nama || "-"}</Text>
-            <Text style={styles.text}>{data?.jabatan || "-"}</Text>
-            <Text style={styles.text}>
-              {data?.client_name + " - " + data?.area || "-"}
-            </Text>
-            <Text style={styles.text}>
-              {dayjs(new Date(data?.periode)).format("MMMM YYYY") || "-"}
-            </Text>
-          </View>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>Nama</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>{data?.nama || "-"}</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.titleItem}>Jabatan</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>{data?.jabatan || "-"}</Text>
+            </GridItem>
+          </Grid>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>Tanggal</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.periode
+                  ? dayjs(new Date(data?.periode)).format("DD/MM/YYYY")
+                  : "-"}
+              </Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.titleItem}>Periode Timesheet</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.timesheet_start || data?.timesheet_end
+                  ? dayjs(new Date(data?.timesheet_start)).format(
+                      "DD/MM/YYYY"
+                    ) +
+                    " s/d " +
+                    dayjs(new Date(data?.timesheet_end)).format("DD/MM/YYYY")
+                  : "-"}
+              </Text>
+            </GridItem>
+          </Grid>
         </View>
         <View style={styles.informationContent}>
-          <View style={{ width: "60%" }}>
-            <Text style={styles.titleItem}>Tanggal</Text>
-            <Text style={styles.titleItem}>Periode Timesheet</Text>
-            <Text style={styles.titleItem}>Total Timesheet</Text>
-            <Text style={styles.titleItem}>Gaji Pokok</Text>
-          </View>
-          <View style={{ width: "40%" }}>
-            <Text style={styles.text}>
-              {dayjs(new Date(data?.periode)).format("DD/MM/YYYY") || "-"}
-            </Text>
-            <Text style={[styles.text, { textTransform: "lowercase" }]}>
-              {dayjs(new Date(data?.timesheet_start)).format("DD/MM/YYYY") +
-                " s/d " +
-                dayjs(new Date(data?.timesheet_end)).format("DD/MM/YYYY")}
-            </Text>
-            <Text style={styles.text}>
-              {data?.total_timesheet_bulan_ini || "0"} Hari
-            </Text>
-            <Text style={styles.text}>
-              {formatIdr(data?.gaji_pokok) || "0"}
-            </Text>
-          </View>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>Lokasi</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.client_name || data?.area
+                  ? data?.client_name + " - " + data?.area
+                  : "-"}
+              </Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.titleItem}>Periode</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.periode
+                  ? dayjs(new Date(data?.periode)).format("MMMM YYYY")
+                  : "-"}
+              </Text>
+            </GridItem>
+          </Grid>
+          <Grid>
+            <GridItem>
+              <Text style={styles.titleItem}>Total Timesheet</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {data?.total_timesheet_bulan_ini || "0"} Hari
+              </Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.titleItem}>Gaji Pokok</Text>
+            </GridItem>
+
+            <GridItem>
+              <Text style={styles.text}>
+                {formatIdr(data?.gaji_pokok) || "0"}
+              </Text>
+            </GridItem>
+          </Grid>
         </View>
       </View>
     </View>
@@ -61,20 +127,15 @@ const EmployeeInfo = ({ data }: EmployeeInfoProps) => {
 
 const styles = StyleSheet.create({
   headerContainer: {
-    marginTop: 16,
-    marginBottom: 20,
+    marginVertical: 14,
   },
   content: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
     marginTop: 6,
   },
   informationContent: {
     flexDirection: "row",
     justifyContent: "space-between",
-    width: "50%",
+    width: "100%",
   },
   txt: {
     fontSize: "10px",
@@ -88,7 +149,6 @@ const styles = StyleSheet.create({
     fontSize: "8px",
     textTransform: "capitalize",
     fontWeight: "bold",
-    marginBottom: 2,
   },
   text: {
     fontWeight: "semibold",
@@ -97,7 +157,17 @@ const styles = StyleSheet.create({
     boxShadow: "none",
     fontSize: "8px",
     textTransform: "capitalize",
-    marginBottom: 2,
+    marginRight: 8,
+    width: "45%",
+  },
+  gridContainer: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  gridItem: {
+    width: "48%",
   },
 });
 
