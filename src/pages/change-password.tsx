7e3f0fe08487da3c9a2d4c493e8433b33b26a23f
ayuputrii <ayuarmadani.pm@gmail.com>
2024-03-26 12:30:16 +0700

    Add change password feature

---
+++ src/pages/change-password.tsx
@@ -0,0 +1,91 @@
+// material-ui
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+} from "@mui/material";
+
+// project import
+import CloseButton from "@/sections/payslip/CloseButton";
+import { useState } from "react";
+import IllustrationSuccess from "../assets/illustration/il-success.svg";
+import IllustrationServerDown from "../assets/illustration/il-server-down.svg";
+import ChangePasswordSections from "@/sections/change-password/ChangePasswordSections";
+import MainCard from "@/components/MainCard";
+
+// ================================|| ChangePassword ||================================ //
+
+const ChangePassword = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(false);
+  const [titleMessage, setTitleMessage] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <MainCard
+      title="Change Password"
+      boxShadow
+      showButton={false}
+      sx={{ minHeight: 250 }}
+    >
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <ChangePasswordSections
+            setError={setError}
+            setShowModal={setShowModal}
+            setTitleMessage={setTitleMessage}
+            setMessage={setMessage}
+          />
+        </Grid>
+      </Grid>
+
+      <Dialog
+        open={showModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{ textAlign: "center" }}
+        fullWidth
+      >
+        <CloseButton onClick={() => setShowModal(false)} />
+        <DialogTitle
+          id="alert-dialog-title"
+          fontWeight={"bold"}
+          color={"steelblue"}
+          marginTop={5}
+        >
+          {titleMessage}
+        </DialogTitle>
+        <DialogContent>
+          <Box marginTop={-4}>
+            {!error ? (
+              <img
+                src={IllustrationSuccess}
+                style={{ width: 250, height: 250 }}
+              />
+            ) : (
+              <img
+                src={IllustrationServerDown}
+                style={{ width: 250, height: 250 }}
+              />
+            )}
+          </Box>
+          <DialogContentText
+            id="alert-dialog-description"
+            style={{
+              color: "gray",
+              fontWeight: "semibold",
+              marginBottom: 20,
+            }}
+          >
+            {message}
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    </MainCard>
+  );
+};
+
+export default ChangePassword;
---
+++ src/sections/change-password/ChangePasswordSections.tsx
@@ -0,0 +1,193 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { Dispatch, MouseEvent, SetStateAction } from "react";
+
+// material-ui
+import {
+  Button,
+  FormHelperText,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+
+// third party
+import * as Yup from "yup";
+import { Formik } from "formik";
+
+// project import
+import IconButton from "@/components/@extended/IconButton";
+import AnimateButton from "@/components/@extended/AnimateButton";
+
+// assets
+import { VisibilityOutlined, VisibilityOffOutlined } from "@mui/icons-material";
+import axios from "axios";
+import { BASE_URL, CHANGE_PASSWORD } from "@/contexts/JWTContext";
+
+// ============================|| JWT - LOGIN ||============================ //
+
+interface ChangePasswordSectionsProps {
+  setError: Dispatch<SetStateAction<boolean>>;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+  setTitleMessage: Dispatch<SetStateAction<string>>;
+  setMessage: Dispatch<SetStateAction<string>>;
+}
+
+const ChangePasswordSections = ({
+  setError,
+  setShowModal,
+  setTitleMessage,
+  setMessage,
+}: ChangePasswordSectionsProps) => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          password: "",
+          submit: null,
+        }}
+        validationSchema={Yup.object().shape({
+          password: Yup.string()
+            .matches(
+              /^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$/,
+              "Password must contain at least one number and one special character"
+            )
+            .min(8)
+            .required("Password is required"),
+        })}
+        onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+          const token = window.localStorage.getItem("serviceToken");
+          const id = window.localStorage.getItem("idEmployee");
+
+          const params = {
+            master_employee_id: id,
+            new_password: values.password,
+          };
+
+          try {
+            const response = await axios.post(
+              BASE_URL + CHANGE_PASSWORD,
+              params,
+              {
+                headers: {
+                  Authorization: token,
+                },
+              }
+            );
+            console.log("response");
+
+            if (response) {
+              setShowModal(true);
+              setError(false);
+              setStatus({ success: true });
+              setSubmitting(false);
+              setTitleMessage(
+                "Congratulations! You have successfully changed your password."
+              );
+              setMessage("Your password has been successfully changed.");
+            }
+          } catch (err: any) {
+            console.log("err", err);
+            setShowModal(true);
+            setError(true);
+            setStatus({ success: false });
+            setErrors({ submit: err.message });
+            setSubmitting(false);
+            setTitleMessage("Sorry, you haven't successfully logged in.");
+            setMessage(
+              err?.data?.message || "Please try again in a few moments!"
+            );
+          }
+        }}
+      >
+        {({
+          errors,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          isSubmitting,
+          touched,
+          values,
+        }) => (
+          <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Stack spacing={1}>
+                  <InputLabel htmlFor="password-login">Password</InputLabel>
+                  <OutlinedInput
+                    fullWidth
+                    error={Boolean(touched.password && errors.password)}
+                    id="-password-login"
+                    type={showPassword ? "text" : "password"}
+                    value={values.password.trim()}
+                    name="password"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                          color="secondary"
+                        >
+                          {showPassword ? (
+                            <VisibilityOutlined />
+                          ) : (
+                            <VisibilityOffOutlined />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    placeholder="Enter password"
+                  />
+                </Stack>
+                {touched.password && errors.password && (
+                  <FormHelperText
+                    error
+                    id="standard-weight-helper-text-password-login"
+                  >
+                    {errors.password}
+                  </FormHelperText>
+                )}
+              </Grid>
+              {errors.submit && (
+                <Grid item xs={12}>
+                  <FormHelperText error>{errors.submit}</FormHelperText>
+                </Grid>
+              )}
+              <Grid item xs={12}>
+                <AnimateButton>
+                  <Button
+                    disableElevation
+                    disabled={isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Change Password
+                  </Button>
+                </AnimateButton>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default ChangePasswordSections;
