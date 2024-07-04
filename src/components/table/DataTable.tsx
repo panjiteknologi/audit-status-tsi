d49db4fd0489f159e7f60c051d2a380ace577e62
ayuputrii <ayuarmadani.pm@gmail.com>
2024-07-04 17:43:13 +0700

    Update Data Table

--- src/components/table/DataTable.tsx
+++ src/components/table/DataTable.tsx
@@ -1,4 +1,5 @@
 import { AllProject } from "@/types/Project";
+import { formatDateTime } from "@/utils/formatDate";
 import {
   Table,
   TableBody,
@@ -7,6 +8,9 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Box,
+  Grid,
+  Typography,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
@@ -62,7 +66,7 @@ const HiddenScrollbarTableContainer = styled(TableContainer)(() => ({
 }));
 
 interface DataTableProps {
-  data?: AllProject[];
+  data?: AllProject[] | null;
 }
 
 const DataTable = ({ data }: DataTableProps) => {
@@ -73,20 +77,100 @@ const DataTable = ({ data }: DataTableProps) => {
           <TableHead>
             <TableRow>
               <StyledTableCell>Tahapan</StyledTableCell>
-              <StyledTableCell>Tanggal Status</StyledTableCell>
+              <StyledTableCell>Tanggal Pelaksanaan</StyledTableCell>
+              <StyledTableCell>SLA</StyledTableCell>
+              <StyledTableCell>Lead Time</StyledTableCell>
               <StyledTableCell>Catatan</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data?.map((row: AllProject, index: number) => (
-              <StyledTableRow key={index}>
-                <BoldTableCell>{row.tahapan}</BoldTableCell>
-                <ScrollableTableCell>
-                  {row?.tanggalStatus || "- "}
-                </ScrollableTableCell>
-                <ScrollableTableCell>{row?.catatan || "-"}</ScrollableTableCell>
-              </StyledTableRow>
-            ))}
+            {data?.map((row: AllProject, index: number) => {
+              const parseTimeString = (timeString: any) => {
+                if (!timeString) {
+                  return {
+                    days: 0,
+                    hours: 0,
+                    minutes: 0,
+                  };
+                }
+
+                const timeParts = timeString.split(" ");
+                let days = 0;
+                let hours = 0;
+                let minutes = 0;
+
+                for (let i = 0; i < timeParts.length; i += 2) {
+                  const value = parseInt(timeParts[i], 10);
+                  const unit = timeParts[i + 1];
+
+                  if (unit && unit.includes("hari")) {
+                    days = value;
+                  } else if (unit && unit.includes("jam")) {
+                    hours = value;
+                  } else if (unit && unit.includes("menit")) {
+                    minutes = value;
+                  }
+                }
+
+                return {
+                  days: days,
+                  hours: hours,
+                  minutes: minutes,
+                };
+              };
+              const timeString = row?.leadTime;
+              const result = parseTimeString(timeString);
+
+              const days = result?.days;
+              const hours = result?.hours;
+              const minutes = result?.minutes;
+
+              return (
+                <StyledTableRow key={index}>
+                  <BoldTableCell>
+                    {row?.tahapan ? row.tahapan : "-"}
+                  </BoldTableCell>
+                  <ScrollableTableCell>
+                    {row?.tanggalStatus
+                      ? formatDateTime(row?.tanggalStatus)
+                      : "-"}
+                  </ScrollableTableCell>
+                  <ScrollableTableCell>
+                    {row?.sla ? formatDateTime(row?.sla) : "-"}
+                  </ScrollableTableCell>
+                  <ScrollableTableCell>
+                    {days || hours || minutes ? (
+                      <Box>
+                        <Grid container>
+                          <Grid item xl={3} xs={4}>
+                            <Typography>
+                              {days ? days + " Hari" : ""}
+                            </Typography>
+                          </Grid>
+                          <Grid item xl={3} xs={4}>
+                            <Typography>
+                              {hours ? hours + " Jam" : ""}
+                            </Typography>
+                          </Grid>
+                          <Grid item xl={3} xs={4}>
+                            <Typography>
+                              {minutes ? minutes + " Menit" : ""}
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                    ) : (
+                      <>
+                        <Typography textAlign="center">-</Typography>
+                      </>
+                    )}
+                  </ScrollableTableCell>
+                  <ScrollableTableCell>
+                    {row?.catatan ? row?.catatan : "-"}
+                  </ScrollableTableCell>
+                </StyledTableRow>
+              );
+            })}
           </TableBody>
         </Table>
       </HiddenScrollbarTableContainer>
