3d0cc594a681569d61d376c8ad2053696a566f3e
ayuputrii <ayuarmadani.pm@gmail.com>
2024-08-08 15:41:36 +0700

    Update dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -87,7 +87,6 @@ const Dashboard = () => {
         setLoading(false);
         return response?.data?.data;
       } catch (error) {
-        console.error("Error fetching projects:", error);
         setLoading(false);
         return [];
       } finally {
@@ -147,12 +146,245 @@ const Dashboard = () => {
     const searchValue = e.target.value.toLowerCase();
     setValue(searchValue);
 
-    const filtered = allProject.filter((item: AllProject) =>
-      Object.values(item).some(
-        (field) =>
+    const filtered = allProject?.filter((item: AllProject) => {
+      const dataFieldAudit: AllProject[] = [
+        {
+          tahapan: "Tanggal Aplication Form or Request",
+          tanggalStatus: item?.tgl_apl_form_or_request,
+          catatan: item?.note_tgl_apl_form_or_request,
+          leadTime: item?.lead_time_tgl_apl_form_or_request,
+        },
+        {
+          tahapan: "Tanggal Review Penugasan ST Satu",
+          tanggalStatus: item?.tgl_review_penugasan_st_satu,
+          catatan: item?.note_tgl_review_penugasan_st_satu,
+          leadTime: item?.lead_time_tgl_review_penugasan_st_satu,
+        },
+        {
+          tahapan: "Tanggal Kontrak",
+          tanggalStatus: item?.tgl_kontrak,
+          catatan: item?.note_tgl_kontrak,
+          leadTime: item?.lead_time_tgl_kontrak,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Notifikasi ST Satu",
+          tanggalStatus: item?.tgl_pengiriman_notif_st_satu,
+          catatan: item?.note_tgl_pengiriman_notif_st_satu,
+          leadTime: item?.lead_time_tgl_pengiriman_notif_st_satu,
+        },
+        {
+          tahapan: "Tanggal Persetujuan Notifikasi ST Satu",
+          tanggalStatus: item?.tgl_persetujuan_notif_st_satu,
+          catatan: item?.note_tgl_persetujuan_notif_st_satu,
+          leadTime: item?.lead_time_tgl_persetujuan_notif_st_satu,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Audit Plan ST Satu",
+          tanggalStatus: item?.tgl_pengiriman_audit_plan_st_satu,
+          catatan: item?.note_tgl_pengiriman_audit_plan_st_satu,
+          leadTime: item?.lead_time_tgl_pengiriman_audit_plan_st_satu,
+        },
+        {
+          tahapan: "Tanggal Pelaksanaan Audit ST Satu",
+          tanggalStatus: item?.tgl_pelaksanaan_audit_st_satu,
+          catatan: item?.note_tgl_pelaksanaan_audit_st_satu,
+          leadTime: item?.lead_time_tgl_pelaksanaan_audit_st_satu,
+        },
+        {
+          tahapan: "Tanggal Penyelesaian CAPA ST Satu",
+          tanggalStatus: item?.tgl_penyelesaian_capa_st_satu,
+          catatan: item?.note_tgl_penyelesaian_capa_st_satu,
+          leadTime: item?.lead_time_tgl_penyelesaian_capa_st_satu,
+        },
+        {
+          tahapan: "Tanggal Review Penugasan Dua",
+          tanggalStatus: item?.tgl_review_penugasan_st_dua,
+          catatan: item?.note_tgl_review_penugasan_st_dua,
+          leadTime: item?.lead_time_tgl_review_penugasan_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Notifikasi Dua",
+          tanggalStatus: item?.tgl_pengiriman_notif_st_dua,
+          catatan: item?.note_tgl_pengiriman_notif_st_dua,
+          leadTime: item?.lead_time_tgl_pengiriman_notif_st_dua,
+        },
+        {
+          tahapan: "Tanggal Persetujuan Notifikasi Dua",
+          tanggalStatus: item?.tgl_persetujuan_notif_st_dua,
+          catatan: item?.note_tgl_persetujuan_notif_st_dua,
+          leadTime: item?.lead_time_tgl_persetujuan_notif_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Audit Plan Dua",
+          tanggalStatus: item?.tgl_pengiriman_audit_plan_st_dua,
+          catatan: item?.note_tgl_pengiriman_audit_plan_st_dua,
+          leadTime: item?.lead_time_tgl_pengiriman_audit_plan_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pelaksanaan Audit Dua",
+          tanggalStatus: item?.tgl_pelaksanaan_audit_st_dua,
+          catatan: item?.note_tgl_pelaksanaan_audit_st_dua,
+          leadTime: item?.lead_time_tgl_pelaksanaan_audit_st_dua,
+        },
+        {
+          tahapan: "Tanggal Penyelesaian CAPA Dua",
+          tanggalStatus: item?.tgl_penyelesaian_capa_st_dua,
+          catatan: item?.note_tgl_penyelesaian_capa_st_dua,
+          leadTime: item?.lead_time_tgl_penyelesaian_capa_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Draft Sertifikat",
+          tanggalStatus: item?.tgl_pengiriman_draft_sertifikat,
+          catatan: item?.note_tgl_pengiriman_draft_sertifikat,
+          leadTime: item?.lead_time_tgl_pengiriman_draft_sertifikat,
+        },
+        {
+          tahapan: "Tanggal Persetujuan Draft Sertifikat",
+          tanggalStatus: item?.tgl_persetujuan_draft_sertifikat,
+          catatan: item?.note_tgl_persetujuan_draft_sertifikat,
+          leadTime: item?.lead_time_tgl_persetujuan_draft_sertifikat,
+        },
+        {
+          tahapan: "Tanggal Pengajuan ke KAN",
+          tanggalStatus: item?.tgl_pengajuan_ke_kan,
+          catatan: item?.note_tgl_pengajuan_ke_kan,
+          leadTime: item?.lead_time_tgl_pengajuan_ke_kan,
+        },
+        {
+          tahapan: "Tanggal Persetujuan KAN",
+          tanggalStatus: item?.tgl_persetujuan_kan,
+          catatan: item?.note_tgl_persetujuan_kan,
+          leadTime: item?.lead_time_tgl_persetujuan_kan,
+        },
+        {
+          tahapan: "Tanggal Kirim Sertifikat",
+          tanggalStatus: item?.tgl_kirim_sertifikat,
+          catatan: item?.note_tgl_kirim_sertifikat,
+          leadTime: item?.lead_time_tgl_kirim_sertifikat,
+        },
+      ];
+
+      const dataField: AllProject[] = [
+        {
+          tahapan: "Tanggal Aplication Form or Request",
+          tanggalStatus: item?.tgl_apl_form_or_request,
+          catatan: item?.note_tgl_apl_form_or_request,
+          leadTime: item?.lead_time_tgl_apl_form_or_request,
+        },
+        {
+          tahapan: "Tanggal Review Penugasan",
+          tanggalStatus: item?.tgl_review_penugasan_st_dua,
+          catatan: item?.note_tgl_review_penugasan_st_dua,
+          leadTime: item?.lead_time_tgl_review_penugasan_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Notifikasi",
+          tanggalStatus: item?.tgl_pengiriman_notif_st_dua,
+          catatan: item?.note_tgl_pengiriman_notif_st_dua,
+          leadTime: item?.lead_time_tgl_pengiriman_notif_st_dua,
+        },
+        {
+          tahapan: "Tanggal Persetujuan Notifikasi",
+          tanggalStatus: item?.tgl_persetujuan_notif_st_dua,
+          catatan: item?.note_tgl_persetujuan_notif_st_dua,
+          leadTime: item?.lead_time_tgl_persetujuan_notif_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Audit Plan",
+          tanggalStatus: item?.tgl_pengiriman_audit_plan_st_dua,
+          catatan: item?.note_tgl_pengiriman_audit_plan_st_dua,
+          leadTime: item?.lead_time_tgl_pengiriman_audit_plan_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pelaksanaan Audit",
+          tanggalStatus: item?.tgl_pelaksanaan_audit_st_dua,
+          catatan: item?.note_tgl_pelaksanaan_audit_st_dua,
+          leadTime: item?.lead_time_tgl_pelaksanaan_audit_st_dua,
+        },
+        {
+          tahapan: "Tanggal Penyelesaian CAPA",
+          tanggalStatus: item?.tgl_penyelesaian_capa_st_dua,
+          catatan: item?.note_tgl_penyelesaian_capa_st_dua,
+          leadTime: item?.lead_time_tgl_penyelesaian_capa_st_dua,
+        },
+        {
+          tahapan: "Tanggal Pengiriman Draft Sertifikat",
+          tanggalStatus: item?.tgl_pengiriman_draft_sertifikat,
+          catatan: item?.note_tgl_pengiriman_draft_sertifikat,
+          leadTime: item?.lead_time_tgl_pengiriman_draft_sertifikat,
+        },
+        {
+          tahapan: "Tanggal Persetujuan Draft Sertifikat",
+          tanggalStatus: item?.tgl_persetujuan_draft_sertifikat,
+          catatan: item?.note_tgl_persetujuan_draft_sertifikat,
+          leadTime: item?.lead_time_tgl_persetujuan_draft_sertifikat,
+        },
+        {
+          tahapan: "Tanggal Pengajuan ke KAN",
+          tanggalStatus: item?.tgl_pengajuan_ke_kan,
+          catatan: item?.note_tgl_pengajuan_ke_kan,
+          leadTime: item?.lead_time_tgl_pengajuan_ke_kan,
+        },
+        {
+          tahapan: "Tanggal Persetujuan KAN",
+          tanggalStatus: item?.tgl_persetujuan_kan,
+          catatan: item?.note_tgl_persetujuan_kan,
+          leadTime: item?.lead_time_tgl_persetujuan_kan,
+        },
+        {
+          tahapan: "Tanggal Kirim Sertifikat",
+          tanggalStatus: item?.tgl_kirim_sertifikat,
+          catatan: item?.note_tgl_kirim_sertifikat,
+          leadTime: item?.lead_time_tgl_kirim_sertifikat,
+        },
+      ];
+
+      const latestProgressAudit = dataFieldAudit
+        .filter((item) => item.tanggalStatus)
+        .sort((a, b) =>
+          new Date(a.tanggalStatus as string) >
+          new Date(b.tanggalStatus as string)
+            ? -1
+            : 1
+        )[0];
+
+      const latestProgress = dataField
+        .filter((item) => item.tanggalStatus)
+        .sort((a, b) =>
+          new Date(a.tanggalStatus as string) >
+          new Date(b.tanggalStatus as string)
+            ? -1
+            : 1
+        )[0];
+
+      return Object.values(item).some((field) => {
+        return (
           typeof field === "string" && field.toLowerCase().includes(searchValue)
-      )
-    );
+        );
+      }) ||
+        item?.standar?.some((standar) => {
+          return Object.values(standar).some((field) => {
+            return (
+              typeof field === "string" &&
+              field.toLowerCase().includes(searchValue)
+            );
+          });
+        }) ||
+        item?.tahapan === 1
+        ? Object.values(latestProgressAudit)?.some((field) => {
+            return (
+              typeof field === "string" &&
+              field.toLowerCase().includes(searchValue)
+            );
+          })
+        : item?.tahapan > 1 &&
+            Object.values(latestProgress)?.some((field) => {
+              return (
+                typeof field === "string" &&
+                field.toLowerCase().includes(searchValue)
+              );
+            });
+    });
     setFilteredData(filtered);
   };
 
@@ -347,11 +579,25 @@ const Dashboard = () => {
       ) : (
         <React.Fragment>
           {filteredData ? (
-            <DashboardSections
-              data={filteredData}
-              setAdd={setAdd}
-              openModal={openModal}
-            />
+            <React.Fragment>
+              <Typography
+                variant="h6"
+                style={{
+                  marginTop: 16,
+                  color: "#000",
+                  fontWeight: "bold",
+                  fontSize: 10,
+                }}
+              >
+                Total: {filteredData?.length ? filteredData?.length : 0} Project
+              </Typography>
+
+              <DashboardSections
+                data={filteredData}
+                setAdd={setAdd}
+                openModal={openModal}
+              />
+            </React.Fragment>
           ) : (
             <Box
               sx={{
