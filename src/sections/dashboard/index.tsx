294c490031e6129918be164efe961570fd947237
Panji Gumelar <panjiggm@gmail.com>
2024-08-26 13:31:59 +0700

    create tabs

--- src/sections/dashboard/index.tsx
+++ src/sections/dashboard/index.tsx
@@ -1,6 +1,24 @@
-import { SetStateAction } from "react";
+import { ReactNode, SetStateAction, SyntheticEvent, useState } from "react";
 import { AllProject } from "@/types/Project";
 import TableInfo from "./TableInfo";
+import ChartInfo from "./ChartInfo";
+import { Box, Tab, Tabs } from "@mui/material";
+import { TableChartRounded, BarChartRounded } from "@mui/icons-material"
+
+function TabPanel({ children, value, index, ...other }: { children: ReactNode, value: number, index: number }) {
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+      {value === index && <Box sx={{ pt: 2 }}>{children}</Box>}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
 
 interface DashboardSectionsProps {
   data: AllProject[] | null;
@@ -11,11 +29,33 @@ interface DashboardSectionsProps {
 const DashboardSections = ({
   data
 }: DashboardSectionsProps) => {
+  const [value, setValue] = useState<number>(0);
+
+  const handleChange = (event: SyntheticEvent<Element, Event>, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
-    <TableInfo
-      data={data || []}
-    />
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Table" icon={<TableChartRounded />} iconPosition="start" {...a11yProps(0)} />
+          <Tab label="Chart" icon={<BarChartRounded />} iconPosition="start" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+
+      {/* Table Panel */}
+      <TabPanel value={value} index={0}>
+        <TableInfo
+          data={data || []}
+        />
+      </TabPanel>
+
+      {/* Chart Panel */}
+      <TabPanel value={value} index={1}>
+        <ChartInfo data={data || []} />
+      </TabPanel>
+    </>
   );
 };
 
