b657695926d47a3d617998cb6407fbf06ba7b84d
Panji Gumelar <panjiggm@gmail.com>
2024-08-27 11:29:46 +0700

    hide tabs

--- src/sections/dashboard/index.tsx
+++ src/sections/dashboard/index.tsx
@@ -4,12 +4,27 @@ import { AllProject } from "@/types/Project";
 import TableInfo from "./TableInfo";
 import ChartInfo from "./ChartInfo";
 import { Box, Tab, Tabs } from "@mui/material";
-import { TableChartRounded, BarChartRounded } from "@mui/icons-material"
+import { TableChartRounded, BarChartRounded } from "@mui/icons-material";
 import CardInfo from "../input/CardInfo";
 
-function TabPanel({ children, value, index, ...other }: { children: ReactNode, value: number, index: number }) {
+function TabPanel({
+  children,
+  value,
+  index,
+  ...other
+}: {
+  children: ReactNode;
+  value: number;
+  index: number;
+}) {
   return (
-    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
       {value === index && <Box sx={{ pt: 2 }}>{children}</Box>}
     </div>
   );
@@ -18,7 +33,7 @@ function TabPanel({ children, value, index, ...other }: { children: ReactNode, v
 function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -28,9 +43,7 @@ interface DashboardSectionsProps {
   setAdd?: (v: boolean) => void;
 }
 
-const DashboardSections = ({
-  data
-}: DashboardSectionsProps) => {
+const DashboardSections = ({ data }: DashboardSectionsProps) => {
   const [open, setOpen] = useState<boolean[]>(Array(data?.length).fill(false));
   const [value, setValue] = useState<number>(0);
 
@@ -40,9 +53,12 @@ const DashboardSections = ({
       newOpen[index] = !newOpen[index];
       return newOpen;
     });
-  }
+  };
 
-  const handleChange = (event: SyntheticEvent<Element, Event>, newValue: number) => {
+  const handleChange = (
+    event: SyntheticEvent<Element, Event>,
+    newValue: number
+  ) => {
     setValue(newValue);
   };
 
@@ -62,28 +78,24 @@ const DashboardSections = ({
         })}
       </MobileView>
       <BrowserView>
-        <TableInfo
-          data={data || []}
-        />
+        <TableInfo data={data || []} />
       </BrowserView>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      {/* <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           <Tab label="Table" icon={<TableChartRounded />} iconPosition="start" {...a11yProps(0)} />
           <Tab label="Chart" icon={<BarChartRounded />} iconPosition="start" {...a11yProps(1)} />
         </Tabs>
-      </Box>
+      </Box> */}
 
       {/* Table Panel */}
-      <TabPanel value={value} index={0}>
-        <TableInfo
-          data={data || []}
-        />
-      </TabPanel>
+      {/* <TabPanel value={value} index={0}>
+        <TableInfo data={data || []} />
+      </TabPanel> */}
 
       {/* Chart Panel */}
-      <TabPanel value={value} index={1}>
+      {/* <TabPanel value={value} index={1}>
         <ChartInfo data={data || []} />
-      </TabPanel>
+      </TabPanel> */}
     </>
   );
 };
