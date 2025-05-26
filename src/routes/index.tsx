df4be61fc3ff157891c734f96805cf55edde3c79
panjiteknologi <itserv.panji@gmail.com>
2025-05-26 11:20:59 +0700

    Define route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Loadable from "@/components/Loadable";
 import AuthLayout from "@/layout/Auth";
 import DashboardLayout from "@/layout/Dashboard";
-import ScopeLibrary from "@/pages/scope-library";
-import DetailProject from "@/pages/detail-project";
+// import ScopeLibrary from "@/pages/scope-library";
+// import DetailProject from "@/pages/detail-project";
 
 const TokenHandler = Loadable(lazy(() => import("@/routes/TokenHandler")));
 const Dashboard = Loadable(lazy(() => import("@/pages/dashboard")));
-const ISO = Loadable(lazy(() => import("@/pages/iso")));
-const ISPO = Loadable(lazy(() => import("@/pages/ispo")));
+// const ISO = Loadable(lazy(() => import("@/pages/iso")));
+// const ISPO = Loadable(lazy(() => import("@/pages/ispo")));
 
 const router = createBrowserRouter(
   [
@@ -34,66 +34,66 @@ const router = createBrowserRouter(
         },
       ],
     },
-    {
-      path: "/iso",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/iso",
-          element: <ISO />,
-        },
-      ],
-    },
-    {
-      path: "/iso-progress",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/iso-progress",
-          element: <ISO />,
-        },
-      ],
-    },
-    {
-      path: "/iso-done",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/iso-done",
-          element: <ISO />,
-        },
-      ],
-    },
-    {
-      path: "/ispo",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/ispo",
-          element: <ISPO />,
-        },
-      ],
-    },
-    {
-      path: "/scope-library",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/scope-library",
-          element: <ScopeLibrary />,
-        },
-      ],
-    },
-    {
-      path: "/detail-project/:id",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/detail-project/:id",
-          element: <DetailProject />,
-        },
-      ],
-    },
+    // {
+    //   path: "/iso",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/iso",
+    //       element: <ISO />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/iso-progress",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/iso-progress",
+    //       element: <ISO />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/iso-done",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/iso-done",
+    //       element: <ISO />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/ispo",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/ispo",
+    //       element: <ISPO />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/scope-library",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/scope-library",
+    //       element: <ScopeLibrary />,
+    //     },
+    //   ],
+    // },
+    // {
+    //   path: "/detail-project/:id",
+    //   element: <DashboardLayout />,
+    //   children: [
+    //     {
+    //       path: "/detail-project/:id",
+    //       element: <DetailProject />,
+    //     },
+    //   ],
+    // },
   ],
   { basename: import.meta.env.VITE_APP_BASE_NAME }
 );
